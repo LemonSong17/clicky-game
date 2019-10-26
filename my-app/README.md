2af0ebe9944abdab20109c44c47055bde1649547
Joe Allen <joe.allen9@gmail.com>
2019-10-26 12:44:40 -0500

    pushing up final version of app

---
+++ my-app/README.md
@@ -0,0 +1,5 @@
+Star Wars Clicky Game
+
+Test your memory with your favorite friends from the beloved saga.
+
+Click on each character once to win the game. But don't click on a character twice, or it's game over for you!
---
+++ my-app/src/components/Jumbotron/Jumbotron.js
@@ -0,0 +1,11 @@
+import React from "react";
+import "./Jumbotron.css";
+
+const Jumbotron = () => (
+  <div className="jumbotron text-center">
+    <h1>The Click Game!</h1>
+    <h3>Click on an image to earn points! If you click on the same picture twice you'll lose!</h3>
+  </div>
+);
+
+export default Jumbotron;
